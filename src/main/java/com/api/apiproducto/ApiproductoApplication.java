08ffc278ecdec15754f9202436c2d082a0e58932
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-06-01 23:05:00 -0400

    Base API Producto

---
+++ src/main/java/com/api/apiproducto/ApiproductoApplication.java
@@ -0,0 +1,13 @@
+package com.api.apiproducto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiproductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiproductoApplication.class, args);
+	}
+
+}
