cabef58a9eb076f665817cb9be19f53e690168d5
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-06-05 21:43:55 -0400

    Se implementaron operaciones CRUD básicas para la entidad Producto con el DTO y repositorio correspondientes

---
+++ src/main/java/com/api/apiproducto/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.api.apiproducto.repository;
+
+import com.api.apiproducto.models.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+}
